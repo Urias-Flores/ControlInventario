202cfc89adb05c8fab1dac5d80b3ff04a1166d24
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2023-01-12 23:00:48 -0600

    Update 11:00 1/12/2023

--- build/generated-sources/ap-source-output/Models/Cotizacion_.java
+++ build/generated-sources/ap-source-output/Models/Cotizacion_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-01-11T18:27:04")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-01-12T22:58:16")
 @StaticMetamodel(Cotizacion.class)
 public class Cotizacion_ { 
 
--- build/generated-sources/ap-source-output/Models/Inventariodetalleacciones_.java
+++ build/generated-sources/ap-source-output/Models/Inventariodetalleacciones_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-01-11T18:27:04")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-01-12T22:58:16")
 @StaticMetamodel(Inventariodetalleacciones.class)
 public class Inventariodetalleacciones_ { 
 
