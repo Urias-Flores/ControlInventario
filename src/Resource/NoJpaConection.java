754b4e86453855cde2af0bb3fce2d63a9cc58415
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2023-02-06 12:48:25 -0600

    Update 12:48 06/02/2023

--- src/Resource/NoJpaConection.java
+++ src/Resource/NoJpaConection.java
@@ -31,7 +31,7 @@ public class NoJpaConection {
     {
         if(conec == null)
         {
-            ip = "192.168.0.100";
+            ip = "127.0.0.1";
             port = "3306";
             user = "root";
             password = "alone2020";
